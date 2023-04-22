7a5ebc2a789c4a47ec150c735c05e39d163cb734
codynego <emonenaabednego@gmail.com>
2023-04-22 23:42:00 +0500

    fixed eslint code

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,14 +1,13 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    if (typeof(size) !== 'number') {
+    if (typeof (size) !== 'number') {
       throw new TypeError('size must be a number');
-    } else if (typeof(location) !== 'string') {
+    } else if (typeof (location) !== 'string') {
       throw new TypeError('location must be a string');
     }
 
     this._size = size;
     this._location = location;
-
   }
 
   valueOf() {
@@ -18,5 +17,4 @@ export default class HolbertonClass {
   toString() {
     return this._location;
   }
-
 }
